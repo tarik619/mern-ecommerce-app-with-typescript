3337a8712496d753c12cec1cbcb8e75cfac54822
tarik619 <itarik707@gmail.com>
2024-01-01 14:06:07 +0600

    creating header component for starter and adding styles on header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loader from "./components/Loader";
+import Header from "./components/Header";
 
 const Home = lazy(() => import("./pages/Home"));
 const Search = lazy(() => import("./pages/Search"));
@@ -29,6 +30,7 @@ const TransactionManagement = lazy(
 export default function App() {
   return (
     <Router>
+      <Header />
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
---
+++ src/components/Header.tsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+import {
+  FaSearch,
+  FaShoppingBag,
+  FaSignInAlt,
+  FaSignOutAlt,
+  FaUser,
+} from "react-icons/fa";
+import { useState } from "react";
+
+export default function Header() {
+  const user = { _id: "fdgfd", role: "admin" };
+  const [isOpen, setIsOpen] = useState(false);
+  const logOutHandler = () => {
+    setIsOpen(false);
+  };
+  return (
+    <nav className="header">
+      <Link onClick={() => setIsOpen(false)} to={"/"}>
+        Home
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/search"}>
+        <FaSearch />
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/cart"}>
+        <FaShoppingBag />
+      </Link>
+      {user?._id ? (
+        <>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            <FaUser />
+          </button>
+          <dialog open={isOpen}>
+            <div className="">
+              {user.role === "admin" && (
+                <Link onClick={() => setIsOpen(false)} to={"/admin/dashboard"}>
+                  Admin
+                </Link>
+              )}
+              <Link onClick={() => setIsOpen(false)} to={"orders"}>
+                Orders
+              </Link>
+              <button onClick={logOutHandler}>
+                <FaSignOutAlt />
+              </button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link to={"/login"}>
+          <FaSignInAlt />
+        </Link>
+      )}
+    </nav>
+  );
+}
