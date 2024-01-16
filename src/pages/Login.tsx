e781fe391cc95e3456c6704457e27da5d5b491e2
tarik619 <itarik707@gmail.com>
2024-01-16 11:58:44 +0600

    creating store.ts for redux and userApi.ts file and making types and api types file and implementing it in login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,15 +4,38 @@ import toast from "react-hot-toast";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 import { auth } from "../firebase";
+import { useLoginMutation } from "../redux/api/userAPI";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/query/react";
+import { MessageResponse } from "../types/api-types";
 
 export default function Login() {
   const [gender, setGender] = useState("");
   const [date, setDate] = useState("");
+  const [login] = useLoginMutation();
 
   const logInHandler = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const { user } = await signInWithPopup(auth, provider);
+
+      const res = await login({
+        name: user.displayName!,
+        email: user.email!,
+        photo: user.photoURL!,
+        gender,
+        role: "user",
+        dob: date,
+        _id: user.uid,
+      });
+
+      if ("data" in res) {
+        toast.success(res.data.message);
+      } else {
+        const error = res.error as FetchBaseQueryError;
+        const message = (error.data as MessageResponse).message;
+        toast.error(message);
+      }
+
       console.log(user);
     } catch (error) {
       toast.error("Sign in fail");
