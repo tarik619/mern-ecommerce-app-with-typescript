e03f346e923851825dba4bbe76f47b67fab0df2e
tarik619 <itarik707@gmail.com>
2024-01-21 21:46:34 +0600

    creating submithandler for payment pay page with stripe

--- src/pages/CheckOut.tsx
+++ src/pages/CheckOut.tsx
@@ -1,6 +1,13 @@
-import { Elements, PaymentElement } from "@stripe/react-stripe-js";
+import {
+  Elements,
+  PaymentElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import toast from "react-hot-toast";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
 const stripePromise = loadStripe(
   "pk_test_51KxlrkKewyYOe5zZcKpOmfE05cfzc5KWHHo9mP2bzJ16sc0bqCaOC2UbFJUfm5KzwUq2zYXSueKMatddKijeHlfG00u7A2Aepy"
@@ -9,22 +16,56 @@ const stripePromise = loadStripe(
 const CheckOutForm = () => {
   const [isProcessing, setIsProcessing] = useState(false);
 
-  const submitHandler = () => {};
+  const stripe = useStripe();
+  const navigate = useNavigate();
+  const elements = useElements();
+
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) return;
+
+    setIsProcessing(true);
+    const orderData = {};
+
+    const { paymentIntent, error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: { return_url: window.location.origin },
+      redirect: "if_required",
+    });
+
+    if (error) {
+      setIsProcessing(false);
+
+      return toast.error(error.message || "something went wrong");
+    }
+    if (paymentIntent?.status === "succeeded") {
+      toast.success("Payment Successful");
+      navigate("/orders");
+    }
+    setIsProcessing(false);
+  };
   return (
     <div className="checkout-container">
       <form onSubmit={submitHandler}>
         <PaymentElement />
-        <button>{isProcessing ? "Processing..." : "Pay"}</button>
+        <button type="submit" disabled={isProcessing}>
+          {isProcessing ? "Processing..." : "Pay"}
+        </button>
       </form>
     </div>
   );
 };
 export default function CheckOut() {
+  const location = useLocation();
+  const clientSecret: string | undefined = location.state;
+
+  if (!clientSecret) <Navigate to={"/shipping"} />;
+
   return (
     <Elements
       options={{
-        clientSecret:
-          "pi_3Ob2UQKewyYOe5zZ008tD45Z_secret_SdBzb3992sWqrDxWMsXIsOJOy",
+        clientSecret,
       }}
       stripe={stripePromise}
     >
